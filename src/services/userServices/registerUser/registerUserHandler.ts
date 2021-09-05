97bbf368a30568195a6358079a4c92a29629f3c2
mahela97 <m.pradeepd@gmail.com>
2021-09-05 15:36:14 +0530

    initial commit

--- src/services/userServices/registerUser/registerUserHandler.ts
+++ src/services/userServices/registerUser/registerUserHandler.ts
@@ -1,10 +1,30 @@
 import { Request, Response } from "express";
 import Joi from "joi";
-import JoiPasswordComplexity from "joi-password-complexity";
 import ServiceLocator from "../../../utill/serviceLocator";
+import UserModel from "../../../models/userModel";
 
 export default class RegisterUserHandler {
   public static async registerUser(req: Request, res: Response): Promise<void> {
-
+ const schema = Joi.object({
+   firstName:Joi.string().required(),
+   lastName:Joi.string().required(),
+   role:Joi.string().required(),
+   email:Joi.string().email().required(),
+   telephone:Joi.string().required(),
+     warehouseId:Joi.string().allow("")
+ });
+ const validate = schema.validate(req.body);
+ if(validate.error){
+   res.status(404).send({message:validate.error.message});
+   
+ }
+ const body:UserModel = validate.value;
+ const userService = ServiceLocator.registerWebUser;
+ try{
+   const result = await userService.registerUser(body);
+   res.status(201).send({message:result});
+ }catch(error){
+   res.status(401).send({message:error.message});
+ }
   }
 }
