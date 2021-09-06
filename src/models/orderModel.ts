9c8fe4b6179539356287f8f1193d0340d7e55fd3
mahela97 <m.pradeepd@gmail.com>
2021-09-06 19:33:58 +0530

    Schemas done

---
+++ src/models/orderModel.ts
@@ -0,0 +1,16 @@
+import ProductObjectModel from "./productObjectModel";
+
+interface OrderModel{
+    variantId:ProductObjectModel[];
+    shopId:string;
+    salespersonId:string;
+    timestamp:string;
+    totalPrice:number;
+    archived:boolean;
+    isPaid:boolean;
+    receivedPrice:number;
+
+}
+
+
+export default OrderModel;
--- src/utill/dBUtil.ts
+++ src/utill/dBUtil.ts
@@ -7,4 +7,14 @@ export default abstract class DBUtil {
 
   static readonly CATEGORIES = "categories";
 
+  static readonly VARIANT = "variants";
+
+  static readonly SHOP = "shops";
+
+  static readonly LEAVE = "leaves";
+
+  static readonly ORDER = "orders";
+
+  static readonly DAILYPRODUCT = "dailyproducts";
+
 }
