02559c9ebadc7ec65c46b08272be4f12d7a69da5
kaveeshadilshan <kaveesha.18@cse.mrt.ac.lk>
2021-09-07 00:49:50 +0530

    ddd

--- src/interfaces/iMapper.ts
+++
@@ -1,7 +0,0 @@
-import { DbModel } from "./dbModel";
-
-export interface IMapper<T extends DbModel> {
-  fromData(data: any): T | undefined;
-
-  toData(item: T): {};
-}
--- src/models/categoryModel.ts
+++
@@ -1,7 +0,0 @@
-interface CategoryModel {
-    cid?: string;
-    name?: string;
-    archived: boolean;
-}
-
-export default CategoryModel;
