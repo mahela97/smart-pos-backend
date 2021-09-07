f0748d8c0adef3c41108c4530fa6af215d1787b0
yasindu-dilshan <56828636+yazi99@users.noreply.github.com>
2021-09-07 16:58:34 +0530

    Shop changes

--- src/dao/shopDAO.ts
+++ src/dao/shopDAO.ts
@@ -1,8 +1,22 @@
 import Shop from "../schemaModels/shop.model";
 import Dao from "../interfaces/dao";
+import QueryHelper from "../utill/QueryHelper";
 
 export default class ShopDAO extends Dao {
     constructor() {
         super(Shop);
+    }
+
+    public async getAll(filterData: Record<string, any>): Promise<Record<string, any>>{
+        const queryHelper = new QueryHelper(
+            filterData.query,
+            ["name"],
+            [],
+            filterData.sortBy,
+            filterData.filter,
+            filterData.page,
+            filterData.limit
+        );
+        return queryHelper.generate(Shop);
     }
 }
