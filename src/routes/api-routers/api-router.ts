bc5b4112337f86df51a30351acde0312cb3a7d45
kaveeshadilshan <kaveesha.18@cse.mrt.ac.lk>
2021-09-10 16:35:43 +0530

    routes are again updated

--- src/routes/api-routers/api-router.ts
+++ src/routes/api-routers/api-router.ts
@@ -3,8 +3,6 @@ import userRouter from "../userRoutes/userRouter";
 import adminRouter from "../adminRoutes/adminRouter";
 import salespersonRouter from "../salespersonRoutes/salespersonRouter";
 import managerRouter from "../managerRoutes/managerRouter";
-import productRouter from "../productRoutes/productRouter";
-import categoryRouter from "../categoryRoutes/categoryRouter";
 
 
 const apiRouter = Router();
@@ -13,7 +11,5 @@ apiRouter.use("/users",userRouter);
 apiRouter.use("/admin",adminRouter);
 apiRouter.use("/salesperson",salespersonRouter);
 apiRouter.use("/manager",managerRouter);
-apiRouter.use("/product",productRouter);
-apiRouter.use("/category",categoryRouter);
 
 export default apiRouter;
