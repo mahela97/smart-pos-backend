61a3370deb9af81411053f0b03626fa9f536b9b5
kaveeshadilshan <kaveesha.18@cse.mrt.ac.lk>
2021-09-18 19:23:51 +0530

    router corrected

--- src/routes/managerRoutes/managerRouter.ts
+++ src/routes/managerRoutes/managerRouter.ts
@@ -2,15 +2,13 @@ import { Router } from "express";
 import productRouter from "./productRouter";
 import categoryRouter from "./categoryRouter";
 import leavesRouter from "./leavesRouter";
+import dailyProductsRouter from "./dailyProductsRouter";
 
 const managerRouter = Router();
 
 managerRouter.use("/product", productRouter);
 managerRouter.use("/category", categoryRouter);
 managerRouter.use("/leaves", leavesRouter);
-
-managerRouter
-  .route("/dailyProducts")
-  .post(AddDailyProductsHandler.addDailyProducts);
+managerRouter.use("/dailyProducts", dailyProductsRouter);
 
 export default managerRouter;
