d15ef8df7a957e0a8d4593558e2655435933a522
mahela97 <m.pradeepd@gmail.com>
2021-09-22 05:39:05 +0530

    sales by incomes salesperson

---
+++ src/services/adminServices/getSalespersonsIncome/getSalespersonsIncomeOrderHandler.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import Joi from "joi";
+import moment from "moment";
+import ServiceLocator from "../../../utill/serviceLocator";
+import { errorResponse } from "../../../utill/responses";
+
+export default class GetSalespersonsIncomeOrderHandler {
+  public static async getAnalytics(req: Request, res: Response): Promise<void> {
+    const querySchema = Joi.object({
+      startDate: Joi.date().default(
+        moment().subtract(5, "days").startOf("day")
+      ),
+      endDate: Joi.date().default(moment().subtract(1, "days").endOf("day")),
+      order: Joi.string().default("dsc"),
+    });
+    const validate = querySchema.validate(req.query);
+    if (validate.error) {
+      res.status(404).send(validate.error.message);
+      return;
+    }
+    const { startDate, endDate, order } = validate.value;
+    const service = ServiceLocator.getSalespersonsIncomeOrderService;
+    try {
+      const result = await service.getIncomeanalytics(
+        startDate,
+        endDate,
+        order
+      );
+
+      res.status(201).send({ incomes: result });
+    } catch (error) {
+      const errorRes = errorResponse(error);
+      res.status(errorRes.code).send(errorRes.message);
+    }
+  }
+}
