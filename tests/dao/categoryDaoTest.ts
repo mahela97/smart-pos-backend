74ef301e5874b591a561597dac7249203f43bd18
kaveeshadilshan <kaveesha.18@cse.mrt.ac.lk>
2021-10-17 18:55:00 +0530

    category test done

--- tests/dao/categoryDaoTest.ts
+++ tests/dao/categoryDaoTest.ts
@@ -1,40 +1,38 @@
-// import CategoryDAO from "../../src/dao/categoryDAO";
-// import chai from "chai";
-// import { expect } from "chai";
-// import { CategoryDocument } from "../../src/schemaModels/category.model";
-//
-// chai.should();
-//
-// describe("CategoryDao Unit Testings", () => {
-//   const categoryDAO = new CategoryDAO();
-//   it("Check Add Category", async () => {
-//     let testCategory: CategoryDocument;
-//     testCategory = await categoryDAO.add({ name: "Test Category" });
-//     console.log(testCategory);
-//     expect(testCategory).to.be.a("object");
-//     after(async () => {
-//       await categoryDAO.delete(testCategory._id);
-//     });
-//   });
-//
-//   it("Check Get All Categories", async () => {
-//     let categoryArray: ;
-//     before(async () => {
-//       await categoryDAO.add({ name: "Test Category One" });
-//       await categoryDAO.add({ name: "Test Category Two" });
-//     });
-//     categoryArray = await categoryDAO.getAll({
-//       query: "",
-//       sortBy: "name",
-//       page: "",
-//       limit: 10,
-//       filter: "",
-//     });
-//     expect(testCategory).to.be.a("object");
-//     after(async () => {
-//       categoryArray.forEach(async (category) => {
-//         await categoryDAO.delete(category._id)
-//       });
-//     });
-//   });
-// });
+import CategoryDAO from "../../src/dao/categoryDAO";
+import chai from "chai";
+import { expect } from "chai";
+import { CategoryDocument } from "../../src/schemaModels/category.model";
+
+chai.should();
+
+describe("CategoryDao Unit Testings", () => {
+  let testCategory: CategoryDocument;
+  let categoryArray: Record<string, any>;
+  const categoryDAO = new CategoryDAO();
+  before(async () => {
+    await categoryDAO.add({ name: "Test Category One" });
+  });
+
+  it("Check Add Category", async () => {
+    testCategory = await categoryDAO.add({ name: "Test Category" });
+    expect(testCategory).to.be.a("object");
+  });
+
+  it("Check Get All Categories", async () => {
+    categoryArray = await categoryDAO.getAll({
+      sortBy: " name",
+      query: "",
+      page: 1,
+      limit: 100,
+      filter: "",
+    });
+    expect(categoryArray.total).to.eql(2);
+    expect(categoryArray.items).to.be.a("array");
+  });
+
+  after(async () => {
+    categoryArray.items.map(async (category: any) => {
+      await categoryDAO.delete(category._id);
+    });
+  });
+});
