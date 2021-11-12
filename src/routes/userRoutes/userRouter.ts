425dbb1fb8c8028d30a23f22bdf3980fee1b7ec8
mahela97 <m.pradeepd@gmail.com>
2021-11-12 13:31:46 +0530

    register

--- src/routes/userRoutes/userRouter.ts
+++ src/routes/userRoutes/userRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import GetCurrentUserHandler from "../../services/userServices/getCurrentUser/GetCurrentUserHandler";
 import UpdateUserLocationHandler from "../../services/userServices/updateUserLocation/updateUserLocationHandler";
+import RegisterUserHandler from "../../services/userServices/registerUser/registerUserHandler";
 
 const userRouter = Router();
 
 userRouter.route("/updateLocation/:salesperson").patch(UpdateUserLocationHandler.updateUserLocation);
 userRouter.route("/me/:uid").get(GetCurrentUserHandler.getCurrentUser);
+userRouter.route("/register").post(RegisterUserHandler.registerUser);
 
 export default userRouter;
